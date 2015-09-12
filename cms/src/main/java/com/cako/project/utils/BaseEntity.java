5e4ff3090b1a26fa144ef97280c45a47fe055530
Curtain <nickhy@126.com>
2015-09-12 19:24:54 +0800

    修改

--- cms/src/main/java/com/cako/project/utils/BaseEntity.java
+++ cms/src/main/java/com/cako/project/utils/BaseEntity.java
@@ -1,27 +1,33 @@
 package com.cako.project.utils;
 
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.MappedSuperclass;
+
 import com.orm.commons.utils.IdEntity;
 import com.orm.enums.SysEnum;
 
+@MappedSuperclass
+@Access(AccessType.PROPERTY)
 public class BaseEntity extends IdEntity {
 
 	protected SysEnum.DeleteStatus deleteStatus = SysEnum.DeleteStatus.NO;
-	protected SysEnum.Display display = SysEnum.Display.DISPLAY;
+	protected SysEnum.Display displayStatus = SysEnum.Display.DISPLAY;
 
 	public SysEnum.DeleteStatus getDeleteStatus() {
 		return deleteStatus;
 	}
 
-	public SysEnum.Display getDisplay() {
-		return display;
+	public SysEnum.Display getDisplayStatus() {
+		return displayStatus;
 	}
 
 	public void setDeleteStatus(SysEnum.DeleteStatus deleteStatus) {
 		this.deleteStatus = deleteStatus;
 	}
 
-	public void setDisplay(SysEnum.Display display) {
-		this.display = display;
+	public void setDisplayStatus(SysEnum.Display displayStatus) {
+		this.displayStatus = displayStatus;
 	}
 
 }
